7dd9279912b3593a221e07db9dc6207ebefee44b
Marwan <maroelsays2013@gmail.com>
2023-12-30 14:50:03 +0200

    added procedures

--- database/petsProcedures.sql
+++ database/petsProcedures.sql
@@ -1,1 +1,64 @@
-SELECT DISTINCT Country FROM Customers;pet
+DELIMITER //
+
+CREATE FUNCTION getDistinctPetSpecies() RETURNS VARCHAR(255) DETERMINISTIC
+BEGIN
+    DECLARE speciesList VARCHAR(1000);
+
+    SELECT GROUP_CONCAT(DISTINCT pet_species SEPARATOR ', ')
+    INTO speciesList
+    FROM pet;
+
+    RETURN speciesList;
+END //
+
+DELIMITER ;
+
+DELIMITER //
+
+CREATE FUNCTION getDistinctPetBreeds() RETURNS VARCHAR(255) DETERMINISTIC
+BEGIN
+    DECLARE breedList VARCHAR(1000);
+
+    SELECT GROUP_CONCAT(DISTINCT pet_breed SEPARATOR ', ')
+    INTO breedList
+    FROM pet;
+
+    RETURN breedList;
+END //
+
+DELIMITER ;
+
+DELIMITER //
+CREATE FUNCTION getDistinctShelterLocations() RETURNS VARCHAR(255) DETERMINISTIC
+BEGIN
+    DECLARE locationList VARCHAR(1000);
+
+    SELECT GROUP_CONCAT(DISTINCT shelter_location SEPARATOR ', ')
+    INTO locationList
+    FROM pet
+    INNER JOIN shelter ON pet.shelter_id = shelter.shelter_id;
+
+    RETURN locationList;
+END;
+DELIMITER ;
+
+DELIMITER //
+
+CREATE PROCEDURE getMinPetAge(OUT minAge INT)
+BEGIN
+    SELECT MIN(pet_age) INTO minAge FROM pet;
+END //
+
+DELIMITER ;
+
+DELIMITER //
+
+CREATE PROCEDURE countShelterRecords(IN shelterId INT, OUT recordCount INT)
+BEGIN
+    SELECT COUNT(*) INTO recordCount
+    FROM shelter
+    WHERE shelter_id = shelterId;
+END //
+
+DELIMITER ;
+
